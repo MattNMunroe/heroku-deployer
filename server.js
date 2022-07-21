d16bd623f45e56b6fa1a434ec857f7e2e5799362
Matt M <Afrodesiacc@gmail.com>
2022-07-21 13:02:05 -0400

    First commit.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+require("dotenv").config();
+
+const PORT = 3003;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+});
+
+app.listen(PORT, () => console.log("Listening on port:", PORT));
+
+module.exports = app;
