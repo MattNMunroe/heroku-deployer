4cd5e094649ec04a9a293c1b5cb7a885babfec12
Matt M <Afrodesiacc@gmail.com>
2022-07-27 23:53:50 -0400

    Amended Server.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const app = require('./app');
 
 require('dotenv').config();
 
-
 const PORT = process.env.PORT || 3003;
 
-app.listen(process.env.PORT || 3003, (() => {
-    console.log(`Listening on port ${process.env.PORT}`)
+app.listen(PORT, (() => {
+    console.log(`Listening on port ${PORT}`)
     }))
